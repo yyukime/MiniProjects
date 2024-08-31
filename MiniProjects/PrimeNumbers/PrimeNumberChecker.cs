87b1a944633cab21985701c49735df2954e45570
yukime <88550743+slushee123@users.noreply.github.com>
2024-08-31 07:24:51 +0200

    k,

---
+++ MiniProjects/PrimeNumbers/PrimeNumberChecker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+
+
+        static void Main()
+        {
+
+            while (true)
+            {
+                try
+                {
+
+                    RunProgram();
+                    break;
+
+                }
+                catch
+                {
+                    Console.Clear();
+                    // Wanted to Console.Writeline($"Your input {stringInput} is not a number, therefore I cant check if its a prime number"); BUT {stringInput} -> not available in context... Why?
+                }
+
+
+
+            }
+        }
+
+        private static void RunProgram()
+        {
+            Console.WriteLine("Input a number to check if it is a prime number:");
+            string? stringInput = Console.ReadLine();
+            int input;
+            input = int.Parse(stringInput);
+
+            if (Logic(input))
+            {
+                Console.WriteLine("The Number is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("The Number is NOT a prime number"); // Could use String interpolation: Console.WriteLine($"The Number {checkNumber} is Not a prime number");
+            }
+        }
+
+        static bool Logic(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }     
+    }    
+}
+
--- MiniProjects/PrimeNumbers/obj/Release/net8.0/PrimeNumbers.AssemblyInfo.cs
+++ MiniProjects/PrimeNumbers/obj/Release/net8.0/PrimeNumbers.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeNumbers")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d5c73e3dd726e655ccee1cb8b48d8f09f8b23d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeNumbers")]
