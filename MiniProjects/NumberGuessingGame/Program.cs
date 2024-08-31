b02bc9eaeec9807bb771dad1c53fd670735eb1e6
yukime <88550743+slushee123@users.noreply.github.com>
2024-08-31 22:00:49 +0200

    Try1

---
+++ MiniProjects/NumberGuessingGame/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//logic:
+// generate a number 
+// turn user input into int
+// check if input matches pre generated number
+
+//program:
+// provide basic text and request user input
+// output result 
+// maybe add a "play again?" button
+
+using System.Security.Cryptography;
+
+
+int[] RNGNumberRange = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+
+Random rng = new();
+rng.Next(10);
+Console.WriteLine(rng.Next());
--- MiniProjects/NumberGuessingGame/obj/Debug/net8.0/NumberGuessingGame.AssemblyInfo.cs
+++ MiniProjects/NumberGuessingGame/obj/Debug/net8.0/NumberGuessingGame.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeNumbers")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2effaeeb8a01c0d426e6d8d32eb5c6ea5fd61896")]
-[assembly: System.Reflection.AssemblyProductAttribute("PrimeNumbers")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PrimeNumbers")]
+[assembly: System.Reflection.AssemblyProductAttribute("NumberGuessingGame")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
