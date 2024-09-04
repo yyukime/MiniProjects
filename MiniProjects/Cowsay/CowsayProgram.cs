8762ca80d5a221d010634d92d94877f0357c0dcb
yukime <88550743+slushee123@users.noreply.github.com>
2024-09-04 17:18:26 +0200

    cowsay

---
+++ MiniProjects/Cowsay/CowsayProgram.cs
@@ -0,0 +1,40 @@
+﻿int input;
+
+while (true)
+{
+    Console.WriteLine("Enter 1 for a surprise");
+    try
+    {
+        input = int.Parse(Console.ReadLine()!);
+
+        if (input == 1)
+        {
+            Cowsay();
+            break;
+        }
+        else
+        {
+            Console.WriteLine("I said press 1 you goof... Press any key to retry");
+            Console.ReadKey();
+
+        }
+        Console.Clear();
+    }
+    catch
+    {
+        Console.Clear();
+    }
+}
+
+static void Cowsay()
+{
+    Console.WriteLine("_________________");
+    Console.WriteLine("< Csharp Rocks >");
+    Console.WriteLine("-----------------");
+    Console.WriteLine();
+    Console.WriteLine("\\    ^__^");
+    Console.WriteLine(" \\   (00) \\______");
+    Console.WriteLine("     (__) \\       )\\/\\");
+    Console.WriteLine("           ||----w|");
+    Console.WriteLine("           ||    ||");
+}
