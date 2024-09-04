03f050e0e4163d64a076fdf1c76b822854739c62
tanoshi <tanoshi40@gmail.com>
2024-09-04 18:32:22 +0200

    revision or how i would have done it i guess?

--- MiniProjects/Schaltjahr/SchaltjahrProgram.cs
+++ MiniProjects/Schaltjahr/SchaltjahrProgram.cs
@@ -1,35 +1,30 @@
-﻿
-int input; // not sexy
+﻿// int input; // not sexy
 
 while (true)
 {
     try
     {
-
         Console.Clear();
         Console.WriteLine("Enter a year to check if it is a leap year");
-        input = int.Parse(Console.ReadLine()!);
+        int input = int.Parse(Console.ReadLine()!);
 
-        if (isLeapYear(input))
-        {
-            Console.Clear(); // look comment under
-            Console.WriteLine($"{input} is a leap year");
-            Console.WriteLine();
-            Console.WriteLine("Press any button to exit");
-            Console.ReadKey();
-            Environment.Exit(0);
-        }
-        else
-        {
-            Console.Clear(); // Better way to remove clutter in console apps?
-            Console.WriteLine($"{input} is not a leap year");
-            Console.WriteLine();
-            Console.WriteLine("Press any button to exit");
-            Console.ReadKey();
-            Environment.Exit(0);
-        }
-        break;
-;
+        Console.Clear();
+
+        string isLeapYearText = IsLeapYear(input) ? $"{input} is a leap year" : $"{input} is not a leap year";
+        Console.WriteLine(isLeapYearText);
+        
+        // if (IsLeapYear(input))
+        // {
+            // Console.WriteLine($"{input} is a leap year");
+        // }
+        // else
+        // {
+            // Console.WriteLine($"{input} is not a leap year");
+        // }
+
+        Console.WriteLine();
+        Console.WriteLine("Press any button to exit");
+        Console.ReadKey();
     }
     catch
     {
@@ -40,13 +35,17 @@ while (true)
     }
 }
 
-static bool isLeapYear (int year)
+static bool IsLeapYear (int year)  // isLeapYear --> IsLeapYear
 {
-    if (year % 4 == 0)
-    {
-        if (year % 100 != 0) // is there not dividable by operator?
-            return true;
-    }
-    return false;
+    // Can be simplified, but not necessarily easier to read
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    
+    
+    // if (year % 4 == 0)
+    // {
+    //     if (year % 100 != 0) // is there not dividable by operator? -> Well % is pretty much the best you get, it's the remainder or rest whatever you wanna call it...
+    //         return true;
+    // }
+    // return false;
 }
 
--- MiniProjects/Verschachtelte Schleife/Schleife(Main).cs
+++ MiniProjects/Verschachtelte Schleife/Schleife(Main).cs
@@ -1,23 +1,25 @@
-﻿int result;
-int num2;
-int num1;
+﻿// int result;
+// int num2;
+// int num1;
 
-for (num1 = 1; num1 <= 10; num1++)
+for (int num1 = 1; num1 <= 10; num1++)
 {
-    for (num2 = 1; num2 < 10; num2++)
+    Console.WriteLine($"{num1}:");
+    for (int num2 = 1; num2 <= 10; num2++)
     {
-        result = num1 * num2;
+        int result = num1 * num2;
         Console.WriteLine($"{num1} x {num2} = {result}");
     }
 
-    result = num1 * num2;
-    Console.WriteLine($"{num1} x {num2} = {result}");
-
-  
+    Console.WriteLine(); // just better overview
+    Console.WriteLine();
+    
+    // result = num1 * num2;
+    // Console.WriteLine($"{num1} x {num2} = {result}");
 }
 
-Console.WriteLine();
+// Console.WriteLine();
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
-System.Environment.Exit(0);
 
+//System.Environment.Exit(0);  // <- why?
