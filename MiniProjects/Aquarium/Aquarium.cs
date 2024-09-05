695e5ce406a295626c2a8a2d4523d74f95b6f0bd
yukime <88550743+slushee123@users.noreply.github.com>
2024-09-05 20:44:55 +0200

    manchmal muss man aufgeben

---
+++ MiniProjects/Aquarium/Aquarium.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium
+{
+    class Wait
+    {
+        static void NumberGenerator()
+        {
+            Random RandomNumber = new Random();
+            int RNGInt = RandomNumber.Next(1);
+            Console.WriteLine(RNGInt);
+
+        }
+
+        static void Main(string[] args)
+        {
+            NumberGenerator();
+        }
+
+
+
+
+
+    }
+}
+
---
+++ MiniProjects/Rennschnecke/Rennschnecke.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace Rennschnecken;
+
+class Rennschnecke
+{
+    public string Snail;
+    public string Race;
+    public int MaxSpeed;
+    // die Schnecke soll wissen welche Strecke sie zurückgelegt hat
+
+    public Rennschnecke() // need to call constructor
+    {
+        Snail = "George";
+        Race = "Black";
+        MaxSpeed = 10;
+        
+    }
+
+    void Kriechen()
+    {
+        Random randomDistance = new();
+        int randomDistanceInt = randomDistance.Next(MaxSpeed);
+        
+
+    }
+    
+    public String toString()
+    {
+        string SnailData = Snail, Race, MaxSpeed;
+        return SnailData;
+    }
+    static void Main(string[] args)
+    {
+        Rennschnecke Schnecke1 = new Rennschnecke();
+        Console.WriteLine($"Snailname: {Schnecke1.Snail}, SnailRace: {Schnecke1.Race}, SnailSpeed: {Schnecke1.MaxSpeed}");
+    }
+
+    
+
+}
+
+class Rennen : Rennschnecke
+{
+    public string NameRennen = "- Rennen der schnellsten Schnecken -";
+    public int AmountOfParticipants; // constructor
+    ArrayList ParticiPantArray = []; 
+    public int DistanceToPass;
+
+     Rennen()
+    {
+        AmountOfParticipants = 5;
+        DistanceToPass = 1500;
+        
+    }
+    
+
+    void addRennschnecken(Rennschnecke Schnecke1)
+    {
+        ParticiPantArray.Add(Schnecke1); // or like above?
+    }
+
+    public string toString()
+    {
+        string RennenData = NameRennen, AmountOfParticipants, ParticiPantArray, DistanceToPass;
+        return RennenData;
+    }
+
+   
+}
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
