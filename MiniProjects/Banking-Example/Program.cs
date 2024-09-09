f6d09ce60c5bd0c97553b2e0967b0c2277ee3015
yukime <88550743+slushee123@users.noreply.github.com>
2024-09-09 19:33:05 +0200

    gjieal43e6jt oi[tjresd

--- MiniProjects/Banking-Example/Program.cs
+++ MiniProjects/Banking-Example/Program.cs
@@ -18,14 +18,8 @@ Bank Bank3 = new("N26", ", 4831-204081923");
 
 while (true)
 {
+    Ui();
 
-    Console.WriteLine("Welcome to the ATM");
-    Console.WriteLine();
-    Console.WriteLine();
-    Console.WriteLine("Press 1 for Norisbank");
-    Console.WriteLine("Press 2 for ING");
-    Console.WriteLine("Press 3 for N26");
-    Console.WriteLine("Please select a Bank:");
     try
     {
         int ConsoleRead = int.Parse(Console.ReadLine()!); // how to ? here? 
@@ -39,6 +33,17 @@ while (true)
 
     Console.ReadLine();
     
+}
+
+void Ui()
+{
+    Console.WriteLine("Welcome to the ATM");
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Press 1 for Norisbank");
+    Console.WriteLine("Press 2 for ING");
+    Console.WriteLine("Press 3 for N26");
+    Console.WriteLine("Please select a Bank:");
 }
 
 void SelectBank(int mode)
