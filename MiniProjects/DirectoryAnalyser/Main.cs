0caa91be09464875d22ea15618e1b1c2a2f5dfe1
yukime <88550743+yyukime@users.noreply.github.com>
2024-10-03 11:37:07 +0200

    Added and Improved:
    Added:
    Analyzer solution file?
    
    Improved:
    DirectoryAnalyzer Code

--- MiniProjects/DirectoryAnalyser/Main.cs
+++ MiniProjects/DirectoryAnalyser/Main.cs
@@ -16,11 +16,13 @@ namespace DirectoryAnalyser
     {
         public static void Main()
         {
+
             StartUI();
         }
 
         static void StartUI()
         {
+            Console.Clear();
             Console.WriteLine("Enter a Dir to analyse:");
             Input();
         }
@@ -28,36 +30,40 @@ namespace DirectoryAnalyser
         {
             string? MethodOutput = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(MethodOutput) == true)
+            if (!string.IsNullOrWhiteSpace(MethodOutput) == true)
+            {
+                try
+                {
+                    AnalyzeAll(MethodOutput);
+                }
+                catch
+                {
+                    Console.WriteLine("Something went wrong... Press any key to try again");
+                    Console.ReadKey();
+                    StartUI();
+                }
+            }
+            else
             {
                 Console.Clear();
                 StartUI();
             }
-            else
-            {
-                AnalyzeForTotalFileCount(MethodOutput);
-            }
         }
 
-        public static void AnalyzeForSubDirectories(string SubDirPath)
+        public static void AnalyzeForSubDirectories(string Path)
         {
-            Console.WriteLine($"You are analyzing the Directory: {SubDirPath}");
-
-            AnalyzeForTotalDirSize(SubDirPath);
-
-            DirectoryInfo dir = new DirectoryInfo(SubDirPath);
+            Console.WriteLine($"You are analyzing the Directory: {Path}");
+            DirectoryInfo dir = new DirectoryInfo(Path);
             DirectoryInfo[] ToDisplay = dir.GetDirectories();
             Console.WriteLine("The number of subdirectories is: " + ToDisplay.Length);
-
-
         }
 
-        public static void AnalyzeForTotalDirSize(string SizePath)
+        public static void AnalyzeForTotalDirSize(string Path)
         {
             long TotalSize = 0;
 
-            DirectoryInfo DirInfo = new DirectoryInfo(SizePath);
-            FileInfo fileInfo = new FileInfo(SizePath);
+            DirectoryInfo DirInfo = new DirectoryInfo(Path);
+            FileInfo fileInfo = new FileInfo(Path);
             DirectoryInfo[] DirList = DirInfo.GetDirectories();
 
             foreach (DirectoryInfo Dir in DirList) // makes list for each step? Inefficient?
@@ -69,22 +75,63 @@ namespace DirectoryAnalyser
                     TotalSize += fileInfo1.Length;
                 }
             }
-
             Console.WriteLine(TotalSize);
-
         }
 
-        public static void AnalyzeForTotalFileCount(string FileCountPath) // Wo ist meine Zeit hin? Und wieso war ich trotz ihr so machtlos? 
+        public static void AnalyzeForTotalFileCount(string Path) // Wo ist meine Zeit hin? Und wieso war ich trotz ihr so machtlos? 
         {
             int FileCount = 0;
-            string[] files = Directory.GetFiles(FileCountPath, "*.*" , SearchOption.AllDirectories);  
+            string[] files = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
             foreach (string file in files)
             {
-                FileCount ++; // oder +=
+                FileCount++; // oder +=
             }
             Console.WriteLine(FileCount);
         }
-             
+
+        public static void AnalyzeAll(string Path)
+        {
+            long TotalSize = 0;
+            int TotalSubDirs = 0;
+            int FileCount = 0;
+            
+
+            FileInfo fileInfo = new FileInfo(Path);
+            DirectoryInfo dirInfo = new DirectoryInfo(Path);
+           
+            DirectoryInfo[] DirList = dirInfo.GetDirectories();
+
+            // Total SubDirs
+            string[] DirSearch = Directory.GetDirectories(Path,"*.*",SearchOption.AllDirectories);
+            foreach (string  dir in DirSearch)
+            {
+                TotalSubDirs ++;
+            }
+
+            // Amount of Files
+            string[] files = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                FileCount++; // oder +=
+            }
+
+            // Total Size
+            string[] SizeSearch = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
+            
+            foreach (string file in SizeSearch)
+            {
+                FileInfo f1 = new FileInfo(file);
+                TotalSize += f1.Length;
+            }
+            
+            // Display 
+            Console.WriteLine($"You are analyzing the Directory: {Path}");
+            Console.WriteLine();
+            Console.WriteLine($"Total Size: {TotalSize} b");
+            Console.WriteLine($"Amount of Files: {FileCount}");
+            Console.WriteLine($"Subdirectories: {TotalSubDirs}");
+        }
+
 
 
 
