59d00f365299133f53b268bc9ba517c92f54b0af
yukime <88550743+yyukime@users.noreply.github.com>
2024-10-09 15:08:07 +0200

    Changes

---
+++ MiniProjects/Current/Hangman/Code.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Hangman;
+
+public class Code
+{
+    public static void Main(string[] args)
+    {
+        //1. Read a File with possible words\
+        //2. Split string into words
+        //3. randomly select a word in array
+        //4. display length with underscore maybe and let player guess a character
+        //5. if character present, unveil or give feedback, if not 
+        //6. if player inputs a word, check if input == word and output result
+        // keep updated live count 
+        // display play again count
+
+
+    }
+}
