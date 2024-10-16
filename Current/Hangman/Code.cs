2ea4f70f2198a6d7e80365b40de76db06235c5ab
yukime <gembajoshua.p@gmail.com>
2024-10-16 11:42:51 +0200

    Started Hangman

--- Current/Hangman/Code.cs
+++ Current/Hangman/Code.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration.Assemblies;
 
 namespace Hangman;
 
@@ -9,7 +10,7 @@ public class Code
 
     }
 
-    public static char Turn()
+    private static char Turn()
     {
         Console.Write("Enter your next guess:");
         string? input = Console.ReadLine();
@@ -18,14 +19,35 @@ public class Code
         char c = Convert.ToChar(input[0]);
         if (!char.IsLetter(c)) throw new Exception("not a letter retard");
         return c;
-        
     }
 
-    public static void ReadFile()
+    private static string[] ReadFile() // maybe give path?
     {
         // either use generic path or select path every time
-        string path;
+        string path = "Examplepath";
         string allText = File.ReadAllText(path);
+        string[] allWords = allText.Split([' ', ',', '\t', '\n', '.', '(', ')', '{', '}', '[', ']', '\r']);
+        return allWords;
+    }
+
+    private static string SelectWord(string[] s)
+    {
+        Random rIndex = new();
+        rIndex.Next(0, s.Length);
+        int index = Convert.ToInt32(rIndex); // why int.parse not work?
+        string word = s[index];
+        if (string.IsNullOrWhiteSpace(word)) throw new ArgumentException("index empty");
+        return word; // word.ToArray();
+    }
+
+    private static void Game(string word)
+    {
+        // Readfile()
+        // SelectWord()
+        if (string.IsNullOrWhiteSpace(word)) throw new ArgumentException("string value may be null");
+        char[] chars = word.ToArray(); // "can be simplified: [.. word]" WHAT THE ...?
 
     }
+
+
 }
