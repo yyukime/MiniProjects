465953d81c086b828a64971e80358b5757a3d1f4
yukime <gembajoshua.p@gmail.com>
2024-10-17 06:39:18 +0200

    continued Hangman

--- Current/Hangman/Code.cs
+++ Current/Hangman/Code.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.ComponentModel;
 using System.Configuration.Assemblies;
+using System.Runtime.CompilerServices;
 
 namespace Hangman;
 
@@ -10,14 +13,45 @@ public class Code
 
     }
 
-    private static char Turn()
+    static public void SelectTurnType(string lowerWord)
+    {
+        while (true)
+        {
+             
+            Console.Clear();
+            char glC = new();
+            bool gW = new();
+            // pls select:
+
+
+            string? stringInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(stringInput)) continue;
+            if (stringInput.Length > 1) continue;
+            if (!stringInput.All(char.IsDigit)) continue;
+            int input = int.Parse(stringInput); // why worse thant !tryparse -> continue;
+
+            // switch statement
+            switch(input)
+            {
+                case 1: glC = CTurn(); break;
+                case 2: gW = GuessWord(lowerWord); break;
+            }
+
+            // char gc present || bool true/false present
+ 
+            List<int> indexes = SubmitChar(lowerWord, glC);
+        }
+
+    }
+    private static char CTurn()
     {
         Console.Write("Enter your next guess:");
         string? input = Console.ReadLine();
         if (string.IsNullOrWhiteSpace(input)) throw new ArgumentException("input is null or empty");
         if (input.Length > 1) throw new Exception("too long");
         char c = Convert.ToChar(input[0]);
-        if (!char.IsLetter(c)) throw new Exception("not a letter retard");
+        if (!char.IsLetter(c)) throw new Exception("not a letter");
+        char.ToLower(c);
         return c;
     }
 
@@ -33,21 +67,42 @@ public class Code
     private static string SelectWord(string[] s)
     {
         Random rIndex = new();
-        rIndex.Next(0, s.Length);
-        int index = Convert.ToInt32(rIndex); // why int.parse not work?
-        string word = s[index];
-        if (string.IsNullOrWhiteSpace(word)) throw new ArgumentException("index empty");
-        return word; // word.ToArray();
+        int index = rIndex.Next(0, s.Length);
+        string sWord = s[index];
+        if (string.IsNullOrWhiteSpace(sWord)) throw new ArgumentException("index empty");
+        string word = sWord.ToLower();
+        return word; // word.ToArray(); In method or in UI
     }
 
-    private static void Game(string word)
+    private static List<int> SubmitChar(string lowerWord, char lowerC) // all, will be given in lowercase
     {
         // Readfile()
         // SelectWord()
-        if (string.IsNullOrWhiteSpace(word)) throw new ArgumentException("string value may be null");
-        char[] chars = word.ToArray(); // "can be simplified: [.. word]" WHAT THE ...?
-
+        List<int> indexes = new();
+        if (string.IsNullOrWhiteSpace(lowerWord)) throw new ArgumentException("string value may be null");
+        char[] index = lowerWord.ToArray();
+        int i = index.Count();
+        for (int a = 0; a < i; i++)
+        {
+            if (index[a] != lowerC) continue;
+            indexes.Add(a);
+        }
+        return indexes;
     }
+
+    private static bool GuessWord(string lowerWord)
+    {
+        while(true)
+        {
+            Console.Clear();
+            Console.Write("Enter your next guess: ");
+            string? input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input)) continue;
+            string lowerGuess = input.ToLower();
+            return lowerWord == lowerGuess;
+        }
+    }
+
 
 
 }
